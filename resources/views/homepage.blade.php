b769023c9b99bc9afb75ff1442032478dd66683d
adilelka06 <adilelka06@hotmail.com>
2024-11-18 11:11:03 +0100

    homepage gefixt

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.navbar')
 
 @section('content')
+    <h1 class="text-4xl font-bold text-blue-600 text-center mb-4">Welcome to the Homepage</h1>
+    <p class="text-gray-700 text-lg text-center">This is the homepage of our school football website. Explore the sections to learn more!</p>
 @endsection
