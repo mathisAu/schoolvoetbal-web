457376a0b19ac46b882d7b0df0bb23ef71fab548
adilelka06 <adilelka06@hotmail.com>
2024-12-09 09:33:45 +0100

    margin h1 hompage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,4 +1,4 @@
 <x-layouts.app>
-    <h1 class="text-4xl font-bold text-blue-600 text-center mb-4">Welcome to the Homepage</h1>
+    <h1 class="text-4xl font-bold text-blue-600 text-center m-9 mb-4">Welcome to the Homepage</h1>
     <p class="text-gray-700 text-lg text-center">This is the homepage of our school football website. Explore the sections to learn more!</p>
 </x-layouts.app>
